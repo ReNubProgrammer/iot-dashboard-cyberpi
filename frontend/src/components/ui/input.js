e4bff7a795083f658d45a518e3b49ee58f8deb69
Owen Nicholas Yap <oyap2552@uni.sydney.edu.au>
2025-01-17 11:55:18 +0700

    deleted react frontend

--- frontend/src/components/ui/input.js
+++
@@ -1,9 +0,0 @@
-// Input Component
-export function Input({ ...props }) {
-    return (
-      <input
-        {...props}
-        className={`w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200 ${props.className}`}
-      />
-    );
-  }
--- frontend/src/components/ui/label.js
+++
@@ -1,8 +0,0 @@
-// Label Component
-export function Label({ children, ...props }) {
-    return (
-      <label {...props} className={`block text-sm font-medium text-gray-700 ${props.className}`}>
-        {children}
-      </label>
-    );
-  }
